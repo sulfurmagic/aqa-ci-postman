756f6f8e66ee0444c2d09417f56500298204d872
sulfurmagic <johnaul81@gmail.com>
2024-07-15 15:57:16 +0300

    added utf-8 charset, fixed error

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -11,6 +11,7 @@ class PostmanEchoTest {
         String text = "приветики";
         given()
                 .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
                 .body(text)
                 .when()
                 .post("/post")
